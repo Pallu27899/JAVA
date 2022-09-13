6630bb59a48ece4a0b49006cf4f11c3362909cfb
Pallavi Bobale <101388809+Pallu27899@users.noreply.github.com>
2022-09-14 02:06:28 +0530

    Update one.java

--- Exercise/one.java
+++ Exercise/one.java
@@ -1,4 +1,5 @@
-// Write a program to calculate the percentage of a given student in the CBSE board exam. His marks from 5 subjects must be taken as input from the keyboard. (Marks are out of 100)
+// Write a program to calculate the percentage of a given student in the CBSE board exam. His marks from 5 subjects must be taken as
+// input from the keyboard. (Marks are out of 100)
 
 package Exercise;
 import java.util.Scanner;
